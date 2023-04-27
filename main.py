669749faec4546da0b3f36beeb409b251fa9df70
justkaram <karamhilles@gmail.com>
2023-04-27 07:15:49 +0200

    Adding .kv and testing stuff

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+Builder.load_file('frontend.kv')
+
 
 class FirstScreen(Screen):
     pass
@@ -14,3 +16,6 @@ class RootWidget(ScreenManager):
 class MainApp(App):
     def build(self):
         return RootWidget()
+
+
+MainApp().run()
