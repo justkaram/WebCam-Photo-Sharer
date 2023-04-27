5a6f38a5c1c28c857789e941d7954d8a6095fb8d
justkaram <karamhilles@gmail.com>
2023-04-27 07:52:00 +0200

    Setting up the main classes for the app

--- main.py
+++ main.py
@@ -2,11 +2,35 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.camera import Camera
+from filestack import Client
 
 Builder.load_file('frontend.kv')
 
 
-class FirstScreen(Screen):
+class CameraScreen(Screen):
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+
+class FileShare:
+
+    def __init__(self, file_path, api_key='AqiHXdRzhT0aW9n3FOeVDz'):
+        self.file_path = file_path
+        self.api_key = api_key
+
+    def gigi(self):
+        client = Client(apikey=self.api_key)
+        link = client.upload(filepath=self.file_path)
+        return link.url
+
+
+class ImageScreen(Screen):
     pass
 
 
