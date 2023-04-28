de11db32cd91dc2dea295b2f689e49a1adf67a35
justkaram <karamhilles@gmail.com>
2023-04-28 14:03:48 +0200

    Improving the size of the widgets

--- main.py
+++ main.py
@@ -22,18 +22,17 @@ class CameraScreen(Screen):
 
     def capture(self):
         current_time = time.strftime('%d%m%Y-%H%M%S')
-        path = f'files/{current_time}.png'
-        self.ids.cam.export_to_png(path)
+        self.path = f'files/{current_time}.png'
+        self.ids.cam.export_to_png(self.path)
         self.manager.current = 'image_screen'
-        self.manager.current_screen.ids.img_screen.source = path
+        self.manager.current_screen.ids.img_screen.source = self.path
 
 
 class ImageScreen(Screen):
     link_message = 'Create a link first !!'
 
     def create_link(self):
-        os.chdir('files')
-        img_path = os.listdir()[-1]
+        img_path = App.get_running_app().root.ids.camera_screen.path
         file = FileShare(file_path=img_path)
         self.img_url = file.get_link()
         self.manager.current_screen.ids.link_label.text = self.img_url
@@ -49,6 +48,9 @@ class ImageScreen(Screen):
             webbrowser.open(self.img_url)
         except AttributeError:
             self.ids.link_label.text = self.link_message
+
+    def back_to_cam(self):
+        self.manager.current = 'camera_screen'
 
 
 class RootWidget(ScreenManager):
